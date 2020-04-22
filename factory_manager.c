82c05ec49887007091e233ad04efeef5028632e8
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-04-23 00:32:51 +0200

    factory_manager reading doc

--- factory_manager.c
+++ factory_manager.c
@@ -52,13 +52,40 @@ int main (int argc, const char * argv[] ) { //command, file_name, num_producers,
 
     FILE* openedFile = fopen(argv[1], O_RDONLY);
 
-    if(openedFile == NULL) perror("File does not exist.");
+    if(openedFile == NULL) perror("File does not exist.\n");
     char* readAmmount;
     int* ammount;
     fgets(readAmmount, 256, openedFile);
     ammount = atoi(readAmmount);
-    if(ammount < argv[2]) perror("Too many producers");
-    
+    if(ammount < argv[2]) perror("There can not be more producers than entries.\n");
+
+    /* Calculate line blocks for producers */
+
+    /* Load file in memory line per line */
+
+    struct element * lines;
+    char * readLine;
+    char * delimiter = " ";
+    for(int i = 0; i < ammount; i++)
+    {
+        char* checkError = fgets(readLine,256,openedFile);
+        if(checkError == NULL && feof(openedFile)) perror("Unexpected EOF.\n");
+        char * elements = strtok(readLine, delimiter);
+        if(i != elements[0]) perror ("Unexpected index mismatch.\n");
+        lines[i].type = elements[1];
+        lines[i].time = elements[2];
+
+    }
+
+    //test
+    for(int i = 0; i < 100; i++){
+        printf("{type: %i, time: %i}\n", lines[i].type, lines[i].time);
+    }
+
+    /* Distribution of lines per producer */
+
+    int threading;
+
     int total = 0;
     printf("Total: %i €.\n", total);
 
