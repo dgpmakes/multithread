1f600be4132ecd8f01055638dfccf5a7315954e8
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-04-22 16:02:28 +0200

    Update costCalculator.c

--- costCalculator.c
+++ costCalculator.c
@@ -24,6 +24,22 @@
  */
 int main (int argc, const char * argv[] ) {
 
+    /*
+        First of all, we check the arguments
+    */
+
+   if(argc != 4) perror("Invalid number of arguments.");
+   
+   /*
+        It is checked that the file exists and
+        the number of producers do not exceed the content data.
+    */
+
+   int descr = fopen(argv[1],'r');
+   if(descr==NULL) perror("Invalid number of arguments.");
+    char* amount;
+    fgets();
+    
     int total = 0;
     printf("Total: %i €.\n", total);
 
